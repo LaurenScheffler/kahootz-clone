a86ce6bdcf116d400d509647827647726c5527bb
LaurenScheffler <Laureninmco@Knights.ucf.edu>
2020-03-04 22:15:58 -0500

    kahoot-clone finish w/sam farls

--- src/App.js
+++ src/App.js
@@ -1,97 +1,105 @@
 import React from 'react';
 import './App.css';
-import QuizQuestion from './components/QuizQuestion'
-import Questions from './questions.json'
+import questions from './questions.json'
 
+const TIME_LIMIT = 5
 const TITLE_STATE = 0
 const QUESTION_STATE = 1
-const TIME_LIMIT = 10
-const FINAL_STATE = 2
+// const FINAL_STATE = 2
 
-class quizQuestion extends React.Component{
-  render() {
-    return <>
-    <h1>{this.props.question}</h1>
-    {this.props.answers.map((v, i ) =>
-    <input onClick ={() => this.props.nextQuestion()} type="button" key={i} value={v.text} />)}
-    </>
+class QuizQuestion extends React.Component {
+render() {
+  return<>
+  <h1>{this.props.question}</h1>
+  {this.props.answers.map((v, i) =>
+  <input className="answerButton" onClick={() => this.props.nextQuestion(v.correct)} type="button" key={i}
+  value={v.text}/>)}
+  </>
   }
 }
-  
+
 class TitlePage extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {titleText: "Welcome to our Quiz!",
-    currentState: TITLE_STATE,
-    counter: 0, 
-    currentQuestion: 0}
-    this.counter = 0
-    this.timeLimit = TIME_LIMIT
-  }
-  nextQuestion() {
-    clearInterval(this.timer)
-    this.setState({
-      titleText: "You answered early!!!",
-      currentState: FINAL_STATE
-    })
-  }
-  countdown(){
-    console.log("Handling interval")
-    console.log(this.state.counter)
-    if(this.state.counter < this.timeLimit){
-      this.setState({
-        titleText : "Starting the Quiz"
-        counter: this.state.counter + 1
-      })
-    } else {
-      this.setState({
-        titleText: "Beginning Quiz!",
-        currentState = QUESTION_STATE,
-        counter: 0
-      })
-      this.timer = setInterva;(() => this.countdown(), 1000)
-      clearInterval(this.timer)
-    }
-  }
-  
-  start(){
-    console.log("Starting!")
-    this.setState({counter:0})
-    this.setState({currentState: QUESTION_STATE})
-    this.timer = setInterval (() => {
-    console.log("INTERVAL CALLED")
-    this.setState({counter : this.state.counter+1})
-    if(this.state.counter < this.timeLimit) {
-      this.setState({titleText: "Begin the Quiz!" + this.state.counter})
-    } else {
-      this.setState({titleText: "Time's up!"})
-      clearInterval(this.Timer)
-    }
-  }, 1000);
-  }
-   render(props){
-    console.log("RENDER CALLED")
-    return(
-      <>
-        <p>{this.timeLimit - this.state.counter}</p>
-        {(this.state.currentState === TITLE_STATE) ? 
-        <QuizQuestion answers={questions[this.state.currentQuestion].possibleAnswers}
-        question={questions[this.state.currentQuestion].question}
-        nextQuestion={() => this.nextQuestion()} />
-        : 
-        <h1 className='title'>{this.state.titleText}</h1>}
-        <input id='startButton' type="button" value="Start" onClick={() => this.start()}></input> 
-      </>
-    )
-  }
+constructor(props) {
+super(props)
+this.state = {
+score: 0,
+titleText: "Welcome to our Quiz!",
+counter: 0,
+currentState: TITLE_STATE,
+currentQuestion: 0
+}
+
+this.timeLimit = TIME_LIMIT
+}
+nextQuestion(correct) {
+console.log("BUTTON PRESSED")
+if(correct){
+this.setState({score: this.state.score+1})
+}
+if(this.state.currentQuestion === questions.length - 1) {
+console.log("DONE")
+} else {
+clearInterval(this.timer)
+console.log(this.state.currentQuestion)
+this.setState({
+titleText:"You answers X",
+currentState: QUESTION_STATE,
+currentQuestion: this.state.currentQuestion +1
+})
+}
+}
+countdown() {
+console.log("Handling interval")
+console.log(this.state.counter)
+if(this.state.counter < this.timeLimit) {
+this.setState({
+titleText: 'Starting the Quiz',
+counter: this.state.counter +1
+})
+} else {
+this.setState({
+titleText: "Beggining Quiz!",
+currentState: QUESTION_STATE,
+counter: 0
+})
+
+if(this.state.currentState === TITLE_STATE) {
+this.timer = setInterval(() => this.countdown(), 1000)
+clearInterval(this.timer)
+} else {
+this.setState({titleText:"You answered!"})
+}
+}
+}
+start() {
+console.log("Starting!")
+this.setState({titleText: "Starting the Quiz!", counter: 0})
+this.timer = setInterval(() => this.countdown(), 1000)
+}
+render() {
+return (
+<>
+<p>{this.timeLimit - this.state.counter}</p>
+{((this.state.currentState === TITLE_STATE) ?
+<>
+<h2>{this.state.titleText}</h2>
+<input className="start" type="button" value="start" onClick={()=>this.start()} />
+</>
+:
+<QuizQuestion answers={questions[this.state.currentQuestion].possibleAnswers} question=
+{questions[this.state.currentQuestion].question} nextQuestion={(correct) => this.nextQuestion(correct)}
+></QuizQuestion>)}
+<p>Score: {this.state.score}</p>
+</>)
+}
 }
 
 function App() {
-  return (
-    <div className="App">
-    <TitlePage></TitlePage>
-    </div>
-  );
+return (
+<div className="App">
+<TitlePage></TitlePage>
+</div>
+);
 }
 
 export default App;

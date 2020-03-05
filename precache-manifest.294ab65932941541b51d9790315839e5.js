40a5718f68922d4efa126b15408b43c068c05f15
LaurenScheffler <Laureninmco@Knights.ucf.edu>
2020-03-04 22:47:15 -0500

    Updates

---
+++ precache-manifest.294ab65932941541b51d9790315839e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d01e06295cbc2261aabfd0e405eefef",
+    "url": "/kahootz-clone/index.html"
+  },
+  {
+    "revision": "22f0105124fac036a46b",
+    "url": "/kahootz-clone/static/css/main.cb68b59f.chunk.css"
+  },
+  {
+    "revision": "b7be0612dc9c10f1b516",
+    "url": "/kahootz-clone/static/js/2.640b199e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kahootz-clone/static/js/2.640b199e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f0105124fac036a46b",
+    "url": "/kahootz-clone/static/js/main.c65f4a41.chunk.js"
+  },
+  {
+    "revision": "33cfa87f1bde22013e2e",
+    "url": "/kahootz-clone/static/js/runtime-main.e9bf6904.js"
+  }
+]);
